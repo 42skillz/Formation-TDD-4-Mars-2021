94446a10883ba326a7ff601f38c6a59a29cc9670
Bruno Boucard <boucard.bruno@free.fr>
2021-03-02 16:42:22 +0100

    StringCalculator updated

--- 01-StringCalculator/src/main/java/StringCalculatorBuilder.java
+++ 01-StringCalculator/src/main/java/StringCalculatorBuilder.java
@@ -4,40 +4,48 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StringCalculatorBuilder {
-    Supplier<Stream<Integer>> streamSupplier;
+    char DEFAULT_DELIMITER = ',';
+    // Supplier needed for stream & exception
+    Supplier<Stream<Integer>> integersSupplier;
 
     public StringCalculatorBuilder() {
     }
 
     public StringCalculatorBuilder extractNumbers(String stringNumbers) {
-        char delimiter = ',';
-
+        char delimiter = DEFAULT_DELIMITER;
         if (stringNumbers.equals("")) stringNumbers = "0";
 
         if (this.hasCustomDelimiter(stringNumbers)) {
             delimiter = this.extractCustomDelimiter(stringNumbers);
             stringNumbers = this.subtractNewStringNumbers(stringNumbers);
         }
-        Integer[] integers = Arrays.stream(stringNumbers.split(String.format("%c|\n", delimiter))).map(Integer::parseInt).toArray(Integer[]::new);
-        this.streamSupplier = () -> Stream.of(integers);
+        Integer[] integers = splitStringNumbersWithDelimiterAndNewLineAsDelimiters(stringNumbers, delimiter)
+                .map(Integer::parseInt).toArray(Integer[]::new);
+        // Supplier needed for stream & exception
+        this.integersSupplier = () -> Stream.of(integers);
         return this;
     }
 
     public StringCalculatorBuilder excludeNumberGreaterThan(int limit) {
-        Integer[] integers = this.streamSupplier.get().filter(n -> n <= limit).toArray(Integer[]::new);
-        this.streamSupplier = () -> Stream.of(integers);
+        Integer[] integers = this.integersSupplier.get().filter(n -> n <= limit).toArray(Integer[]::new);
+        // Supplier needed for stream & exception
+        this.integersSupplier = () -> Stream.of(integers);
         return this;
     }
 
     public StringCalculatorBuilder handleNegativesNumbers() throws Exception {
-        if (this.getNegativesStream(streamSupplier).count() > 0) {
+        if (this.getNegativesNumbers(integersSupplier).count() > 0) {
             this.throwNegativesNotAllowed();
         }
         return this;
     }
 
     public Integer sum() {
-        return this.streamSupplier.get().reduce(Integer::sum).orElse(0);
+        return this.integersSupplier.get().reduce(Integer::sum).orElse(0);
+    }
+
+    private Stream<String> splitStringNumbersWithDelimiterAndNewLineAsDelimiters(String stringNumbers, char delimiter) {
+        return Arrays.stream(stringNumbers.split(String.format("%c|\n", delimiter)));
     }
 
     private String subtractNewStringNumbers(String stringNumbers) {
@@ -54,10 +62,10 @@ public class StringCalculatorBuilder {
 
     private void throwNegativesNotAllowed() throws Exception {
         throw new Exception(String.format("negatives not allowed:%s",
-                getNegativesStream(this.streamSupplier).map(Object::toString).collect(Collectors.joining(","))));
+                getNegativesNumbers(this.integersSupplier).map(Object::toString).collect(Collectors.joining(","))));
     }
 
-    private Stream<Integer> getNegativesStream(Supplier<Stream<Integer>> streamIntegers) {
+    private Stream<Integer> getNegativesNumbers(Supplier<Stream<Integer>> streamIntegers) {
         return streamIntegers.get().filter(this::isNegative);
     }
 
